71e017b6bf3dde1c6d296355cbf3ff52618a743f
sahlminkok <sahlminkok7@gmail.com>
2023-06-14 14:57:08 +0100

    Add calculator skeleton with css

---
+++ src/components/calculator.js
@@ -0,0 +1,70 @@
+import './calculator.css';
+
+const Calculator = () => (
+  <section className="calculator">
+    <div className="operations">
+      <div className="result grayColor">
+        <h1>1234567</h1>
+      </div>
+      <div className="grayColor">
+        <p>AC</p>
+      </div>
+      <div className="grayColor">
+        <p>+/-</p>
+      </div>
+      <div className="grayColor">
+        <p>%</p>
+      </div>
+      <div className="orangeColor">
+        <p>/</p>
+      </div>
+      <div className="grayColor">
+        <p>7</p>
+      </div>
+      <div className="grayColor">
+        <p>8</p>
+      </div>
+      <div className="grayColor">
+        <p>9</p>
+      </div>
+      <div className="orangeColor">
+        <p>X</p>
+      </div>
+      <div className="grayColor">
+        <p>4</p>
+      </div>
+      <div className="grayColor">
+        <p>5</p>
+      </div>
+      <div className="grayColor">
+        <p>6</p>
+      </div>
+      <div className="orangeColor">
+        <p>-</p>
+      </div>
+      <div className="grayColor">
+        <p>1</p>
+      </div>
+      <div className="grayColor">
+        <p>2</p>
+      </div>
+      <div className="grayColor">
+        <p>3</p>
+      </div>
+      <div className="orangeColor">
+        <p>+</p>
+      </div>
+      <div className="twoTimesWidth grayColor">
+        <p>0</p>
+      </div>
+      <div className="grayColor">
+        <p>.</p>
+      </div>
+      <div className="orangeColor">
+        <p>=</p>
+      </div>
+    </div>
+  </section>
+);
+
+export default Calculator;
