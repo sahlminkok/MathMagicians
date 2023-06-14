aa6e8c83b39f773b84f56a46f61697efb10f74c4
sahlminkok <sahlminkok7@gmail.com>
2023-06-14 17:00:57 +0100

    Create a child component

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import Calculator from './components/calculator';
+
+const App = () => (
+  <div className="App">
+    <Calculator />
+  </div>
+);
+
+export default App;
--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,29 +1,9 @@
 import './calculator.css';
+import Operations from './operations';
 
 const Calculator = () => (
   <section className="calculator">
-    <div className="operations">
-      <div className="result darkGray">0</div>
-      <div className="grayColor">AC</div>
-      <div className="grayColor">+/-</div>
-      <div className="grayColor">%</div>
-      <div className="orangeColor">/</div>
-      <div className="grayColor">7</div>
-      <div className="grayColor">8</div>
-      <div className="grayColor">9</div>
-      <div className="orangeColor">X</div>
-      <div className="grayColor">4</div>
-      <div className="grayColor">5</div>
-      <div className="grayColor">6</div>
-      <div className="orangeColor">-</div>
-      <div className="grayColor">1</div>
-      <div className="grayColor">2</div>
-      <div className="grayColor">3</div>
-      <div className="orangeColor">+</div>
-      <div className="twoTimesWidth grayColor">0</div>
-      <div className="grayColor">.</div>
-      <div className="orangeColor">=</div>
-    </div>
+    <Operations />
   </section>
 );
 
