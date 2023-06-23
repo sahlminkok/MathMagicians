56151430833c5720e19bd0c6f9276a005e022b4e
porag-m06 <porag.m06@gmail.com>
2023-06-23 23:17:28 +0600

    update

--- src/__test__/calculator.test.js
+++ src/__test__/calculator.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-// import { render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Calculator from '../components/calculator';
 
@@ -9,5 +9,10 @@ describe('Calculator', () => {
   test('Calculator Component renders correctly', () => {
     const tree = renderer.create(<Calculator />).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  test('Renders do some Math! text', () => {
+    render(<Calculator />);
+    expect(screen.getByText(/do some Math!/)).toBeInTheDocument();
   });
 });
