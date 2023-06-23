75f2af14cb3a86a7c957c260f3983298cbac1f33
porag-m06 <porag.m06@gmail.com>
2023-06-23 22:25:59 +0600

    fixed eslint issue and added test for home, calculator and result

--- src/__test__/calculator.test.js
+++ src/__test__/calculator.test.js
@@ -1,5 +1,13 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+// import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import Calculator from '../components/calculator';
 
-test('initial test setup check', () => {
-  expect('Calculator').toBe('Calculator');
+describe('Calculator', () => {
+  // Jest: tests with snapshots
+  test('Calculator Component renders correctly', () => {
+    const tree = renderer.create(<Calculator />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
